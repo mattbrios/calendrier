dfccadc3f2fa4b65ec6428c82f5565554ccde1fa
mattbrios@gmail.com <mattbrios@gmail.com>
2020-09-20 20:29:40 -0300

    Finalizando a Home, adicionando a página de Funcionalidades e de Planos

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@ $(function () {
     $('#menu').load('../menu.html');
     $('#footer').load('../footer.html');
 
+    $('.btn-saibamais').click(function() {
+        window.location.href = "funcionalidades.html"
+    });
+
+    $('.btn-scroll').click( function(e) {
+        e.preventDefault();
+        scrollTo($(this).attr('destiny'));
+    });
+    
     $('.btn-video').click(function() {
         
     });
 });
+
+function scrollTo(destiny) {
+    $('html, body').animate({
+        scrollTop: $('#' + destiny).offset().top
+    }, 'slow');
+}
